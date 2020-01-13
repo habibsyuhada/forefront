ae952257f2eca87d06945f6deff57612faae092a
Habib Syuhada <habibsumedang@gmail.com>
2020-01-13 08:49:00 +0700

    Set Up

---
+++ application/views/_partial/footer.php
@@ -0,0 +1,62 @@
+	<!--================ Start Footer Area =================-->
+	<footer class="footer_area py-4 wow fadeIn">
+		<div class="container">
+			<div class="row footer_inner justify-content-center">
+				<div class="col-lg-12 text-center">
+					<aside class="f_widget social_widget">
+						<div class="f_logo">
+							<img src="<?php echo base_url(); ?>assets/img/logo.png" alt="">
+						</div>
+						<!-- <div class="f_title">
+							<h4>Follow Me</h4>
+						</div>
+						<ul class="list">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+							<li><a href="#"><i class="fa fa-behance"></i></a></li>
+						</ul> -->
+					</aside>
+					<div class="pt-3">
+						<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | <i class="fas fa-heart"></i> from <a href="#" target="_blank">Smart Pro Training</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!--================End Footer Area =================-->
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="<?php echo base_url(); ?>assets/js/jquery-3.2.1.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/popper.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/stellar.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/jquery.magnific-popup.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/vendors/isotope/imagesloaded.pkgd.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/vendors/isotope/isotope-min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/vendors/animate-css/wow.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/vendors/owl-carousel/owl.carousel.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/jquery.ajaxchimp.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/mail-script.js"></script>
+	<!--gmaps Js-->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+	<script src="<?php echo base_url(); ?>assets/js/gmaps.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/theme.js"></script>
+
+	<script type="text/javascript">
+		wow = new WOW({
+      boxClass:     'wow',      // default
+      animateClass: 'animated', // default
+      offset:       10,          // default
+      mobile:       true,       // default
+      live:         true        // default
+    });
+    wow.init();
+	</script>
+</body>
+
+</html>
---
+++ application/views/home/index.php
@@ -0,0 +1,292 @@
+	<!--================Home Banner Area =================-->
+	<section class="wow fadeIn" data-wow-duration="2s" id="home"  style="background: url(<?php echo base_url() ?>assets/img/banner/banner_1.png);">
+		<div class="">
+			<div class="container-fluid">
+				<div class="row fullscreen d-flex align-items-center">
+					<div class="banner-content col-lg-12 justify-content-center">
+						<h1 style="color: #222222">Cara cerdas untuk Meningkatkan Keahlian anda</h1>
+						<h3 style="color: #222222">Melayani semua jenis training, kapan dan dimanapun anda berada, atau ikuti puluhan training regular kami di tiap akhir pekan di berbagai kota. baik untuk individu maupun perusahaan.</h3>
+						<a href="#" class="primary-btn font-weight-bold"><i class="fas fa-list-alt"></i> &nbsp; Daftar Training</a>
+						<a href="#" class="primary-btn font-weight-bold"><i class="fas fa-users"></i> &nbsp; Ingin Menjadi Trainer?</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End Home Banner Area =================-->
+
+	<!--================ Start Keunggulan =================-->
+	<section class="about-area section_gap gray-bg">
+		<div class="container-fluid">
+			<div class="main-title text-left wow fadeInDown" data-wow-duration="1.5s">
+				<h1>KEUNGGULAN KAMI</h1>
+			</div>
+			<div class="wow fadeInRight" data-wow-duration="1.5s">
+				<div class="row justify-content-between d-flex align-items-stretch bg-white card-advantage">
+					<div class="col-lg col-md-6 col-sm-12 py-4 text-center">
+						<div class="mb-50">
+							<h1 class="text-dark"><i class="fas fa-user-tie"></i></h1>
+							<h3 class="text-dark">Profesional</h3>
+							<span class="text-left">Trainer kami dari praktisi profesional dibidangnya.</span>
+						</div>
+					</div>
+					<div class="col-lg col-md-6 col-sm-12 py-4 text-center">
+						<div class="mb-50">
+							<h1 class="text-dark"><i class="fas fa-address-book"></i></h1>
+							<h3 class="text-dark">Mudah</h3>
+							<span class="text-left">Pendaftaran mudah secara online</span>
+						</div>
+					</div>
+					<div class="col-lg col-md-6 col-sm-12 py-4 text-center">
+						<div class="mb-50">
+							<h1 class="text-dark"><i class="fas fa-hourglass-half"></i></h1>
+							<h3 class="text-dark">Efektif</h3>
+							<span class="text-left">Waktu training yang singkat dan padat</span>
+						</div>
+					</div>
+					<div class="col-lg col-md-6 col-sm-12 py-4 text-center">
+						<div class="mb-50">
+							<h1 class="text-dark"><i class="fas fa-book"></i></h1>
+							<h3 class="text-dark">Lengkap</h3>
+							<span class="text-left">Kurikulum materi disusun secara seksama</span>
+						</div>
+					</div>
+					<div class="col-lg col-md-6 col-sm-12 py-4 text-center">
+						<div class="mb-50">
+							<h1 class="text-dark"><i class="fas fa-clock"></i></h1>
+							<h3 class="text-dark">Flexible</h3>
+							<span class="text-left">Flexible dalam menentukan waktu training</span>
+						</div>
+					</div>
+				</div>
+				<div class="row justify-content-between d-flex align-items-stretch bg-white card-advantage">
+					<div class="col-lg col-md-6 col-sm-12 py-4 text-center">
+						<div class="mb-50">
+							<h1 class="text-dark"><i class="fas fa-dollar-sign"></i></h1>
+							<h3 class="text-dark">Terjangkau</h3>
+							<span class="text-left">Biaya yang lebih ekonomis karena kami tidak memiliki biaya harian seperti training konvensional</span>
+						</div>
+					</div>
+					<div class="col-lg col-md-6 col-sm-12 py-4 text-center">
+						<div class="mb-50">
+							<h1 class="text-dark"><i class="fas fa-credit-card"></i></h1>
+							<h3 class="text-dark">Pembayaran</h3>
+							<span class="text-left">Pembayaran dilakukan secara transfer.</span>
+						</div>
+					</div>
+					<div class="col-lg col-md-6 col-sm-12 py-4 text-center">
+						<div class="mb-50">
+							<h1 class="text-dark"><i class="fas fa-medal"></i></h1>
+							<h3 class="text-dark">Sertifikat</h3>
+							<span class="text-left">Sertifikat dari smartpro training</span>
+						</div>
+					</div>
+					<div class="col-lg col-md-6 col-sm-12 py-4 text-center">
+						<div class="mb-50">
+							<h1 class="text-dark"><i class="fas fa-map-marked-alt"></i></h1>
+							<h3 class="text-dark">Lokasi</h3>
+							<span class="text-left">Tempat training di berbagai kota di Indonesia</span>
+						</div>
+					</div>
+					<!-- <div class="col-lg col-md-6 col-sm-12 py-4 text-center">
+						<div class="mb-50">
+							<h1><i class="fas fa-map-marked-alt"></i></h1>
+							<h3>Lokasi</h3>
+							<span class="text-left">Tempat training di berbagai kota di Indonesia</span>
+						</div>
+					</div> -->
+				</div>
+				
+			</div>
+		</div>
+	</section>
+	<!--================ End Keunggulan =================-->
+
+	<!--================ Start About Area =================-->
+	<section class="section_gap">
+		<div class="container-fluid">
+			<div class="row justify-content-between">
+				<div class="col-lg-12 text-center wow fadeInDown">
+					<div class="main-title">
+						<h1>PRODUK KAMI</h1>
+					</div>					
+				</div>
+				<div class="col-lg-6 col-md-12 wow fadeInLeft">
+					<img class="img-fluid" src="<?php echo base_url(); ?>assets/img/Chart_1.png" alt="">
+					<h1 class="text-center text-dark">TRAINING KAMI</h1>
+					<div class="mb-50">
+						<p>
+							Training  kami, mencakup berbagai industri seperti, minyak dan gas, bangunan dan konstruksi, manufaktur, galangan kapal, pengembangan diri dan lainnya.
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-6 col-md-12 wow fadeInRight">
+					<img class="img-fluid" src="<?php echo base_url(); ?>assets/img/Chart_1.png" alt="">
+					<h1 class="text-center text-dark">JADWAL TRAINING</h1>
+					<div class="mb-50">
+						<p>
+							Ikuti training regular kami yang rutin diadakan di kota kota di seluruh indonesia, lihat daftar training dan cari training yang sesuai dengan keperluan anda.
+						</p>
+						<p>
+							Jadwal training sesuai permintaan disesuaikan dengan kebutuhan anda. kapan dan dimanapun anda berada
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================ End About Area =================-->
+
+	<!--================ Start About Area =================-->
+	<section class="about-area section_gap gray-bg">
+		<div class="container-fluid">
+			<div class="row align-items-center justify-content-between">
+				<div class="col-lg-5 about-left wow fadeInDown">
+					<img class="img-fluid" src="<?php echo base_url(); ?>assets/img/Chart_1.png" alt="">
+				</div>
+				<div class="col-lg-6 col-md-12 about-right">
+					<div class="main-title text-left wow fadeInDown">
+						<h1>KETENTUAN TRAINING</h1>
+					</div>
+					<div class="mb-50 wow fadeInRight">
+						<ul class="unordered-list">
+							<li>Smartpro training adalah penyedia layanan training dalam menyediakan Trainer, Modul training dan tempat training.</li>
+							<li>Untuk training yang berkaitan dengan applikasi komputer, smartpro training tidak menyediakan komputer atau laptop beserta applikasi program yang diperlukan dalam training.</li>
+							<li>Peserta harus mempersiapkan sendiri komputer/Laptop beserta program aplikasi yang diperlukan didalamnya.</li>
+							<li>Training akan berjalan jika peserta minimal sudah tercapai, sudah melakukan pendaftaran dan membayar biaya training. untuk training regular.</li>
+							<li>Pembayaran hanya dilakukan melalui transfer ke Bank Mandiri, atas nama RAIDI GROUP dengan nomor rekening 109.00.138.184.55</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================ End About Area =================-->
+
+	<!--================ Start Testimonial Area =================-->
+	<!-- <div class="section_gap testimonial_area">
+		<div class="container-fluid">
+			<div class="row justify-content-center">
+				<div class="col-lg-7 text-left">
+					<div class="active_testimonial owl-carousel" data-slider-id="1">
+						
+						<div class="single_testimonial">
+							<div class="testimonial_head">
+								<img src="<?php echo base_url(); ?>assets/img/quote.png" alt="">
+								<h4>Fanny Spencer</h4>
+								<div class="review">
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+								</div>
+							</div>
+							<div class="testimonial_content">
+								<p>As conscious traveling Paup ers we must always be oncerned about our dear Mother Earth. If you think about
+									it, you travel across her face</p>
+							</div>
+						</div>
+
+						<div class="single_testimonial">
+							<div class="testimonial_head">
+								<img src="<?php echo base_url(); ?>assets/img/quote.png" alt="">
+								<h4>Fanny Spencer</h4>
+								<div class="review">
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+								</div>
+							</div>
+							<div class="testimonial_content">
+								<p>As conscious traveling Paup ers we must always be oncerned about our dear Mother Earth. If you think about
+									it, you travel across her face</p>
+							</div>
+						</div>
+
+						<div class="single_testimonial">
+							<div class="testimonial_head">
+								<img src="<?php echo base_url(); ?>assets/img/quote.png" alt="">
+								<h4>Fanny Spencer</h4>
+								<div class="review">
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+								</div>
+							</div>
+							<div class="testimonial_content">
+								<p>As conscious traveling Paup ers we must always be oncerned about our dear Mother Earth. If you think about
+									it, you travel across her face</p>
+							</div>
+						</div>
+
+						<div class="single_testimonial">
+							<div class="testimonial_head">
+								<img src="<?php echo base_url(); ?>assets/img/quote.png" alt="">
+								<h4>Fanny Spencer</h4>
+								<div class="review">
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+								</div>
+							</div>
+							<div class="testimonial_content">
+								<p>As conscious traveling Paup ers we must always be oncerned about our dear Mother Earth. If you think about
+									it, you travel across her face</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 offset-lg-1">
+					<div class="testimonial_logos">
+						<div class="top_logos">
+							<img src="<?php echo base_url(); ?>assets/img/brands/logo1.png" alt="">
+							<img src="<?php echo base_url(); ?>assets/img/brands/logo2.png" alt="">
+						</div>
+						<div class="mid_logo">
+							<img src="<?php echo base_url(); ?>assets/img/brands/logo3.png" alt="">
+						</div>
+						<div class="bottom_logos jus">
+							<img src="<?php echo base_url(); ?>assets/img/brands/logo4.png" alt="">
+							<img src="<?php echo base_url(); ?>assets/img/brands/logo5.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div> -->
+	<!--================ End Testimonial Area =================-->
+
+	<!--================ Start Newsletter Area =================-->
+	<section class="section_gap newsletter-area" style="background-attachment: fixed;background-position: center;">
+		<div class="container-fluid">
+			<div class="row justify-content-center">
+				<div class="col-lg-6 text-center wow fadeIn" data-wow-duration="1.5s">
+					<div class="main-title">
+						<h1>Ingin informasi lagi? Hubungi Kami</h1>
+					</div>
+					<p class="text-white">Kami disini untuk membantu anda, hubungi kami melalui telepon, email atau sosial menyediakan.</p>
+					<a href="<?php echo base_url() ?>contact" class="primary-btn"><i class="fas fa-phone"></i> &nbsp; Hubungi Kami</a>
+
+					<aside class="f_widget social_widget">
+						<div class="f_title">
+							<h4 class="text-white">Social Media</h4>
+						</div>
+						<ul class="list">
+							<li><a href="#"><i class="fab fa-facebook-square text-white"></i></a></li>
+							<li><a href="#"><i class="fab fa-twitter-square text-white"></i></a></li>
+							<li><a href="#"><i class="fab fa-instagram text-white"></i></a></li>
+							<li><a href="#"><i class="fab fa-linkedin text-white"></i></a></li>
+						</ul>
+					</aside>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================ End Newsletter Area =================-->
