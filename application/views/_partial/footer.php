95543d456ad1f1ed4466733aa79598b08ef7c036
Habib Syuhada <habibsumedang@gmail.com>
2020-01-13 18:47:09 +0700

    .

--- application/views/_partial/footer.php
+++ application/views/_partial/footer.php
@@ -21,18 +21,6 @@
 
   <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
 
-  <!-- JavaScript Libraries -->
-  <script src="<?php echo base_url(); ?>assets/lib/jquery/jquery.min.js"></script>
-  <script src="<?php echo base_url(); ?>assets/lib/jquery/jquery-migrate.min.js"></script>
-  <script src="<?php echo base_url(); ?>assets/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="<?php echo base_url(); ?>assets/lib/easing/easing.min.js"></script>
-  <script src="<?php echo base_url(); ?>assets/lib/superfish/hoverIntent.js"></script>
-  <script src="<?php echo base_url(); ?>assets/lib/superfish/superfish.min.js"></script>
-  <script src="<?php echo base_url(); ?>assets/lib/wow/wow.min.js"></script>
-  <script src="<?php echo base_url(); ?>assets/lib/owlcarousel/owl.carousel.min.js"></script>
-  <script src="<?php echo base_url(); ?>assets/lib/magnific-popup/magnific-popup.min.js"></script>
-  <script src="<?php echo base_url(); ?>assets/lib/sticky/sticky.js"></script>
-
   <script type="text/javascript">
     $(document).ready(function (){
         // Stick the header at top on scroll
--- application/views/_partial/header.php
+++ application/views/_partial/header.php
@@ -34,4 +34,17 @@
     Author: BootstrapMade.com
     License: https://bootstrapmade.com/license/
   ======================================================= -->
+
+  <!-- JavaScript Libraries -->
+  <script src="<?php echo base_url(); ?>assets/lib/jquery/jquery.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/lib/jquery/jquery-migrate.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/lib/easing/easing.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/lib/superfish/hoverIntent.js"></script>
+  <script src="<?php echo base_url(); ?>assets/lib/superfish/superfish.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/lib/wow/wow.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/lib/magnific-popup/magnific-popup.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/lib/sticky/sticky.js"></script>
+  
 </head>
